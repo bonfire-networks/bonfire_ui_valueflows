8c6950e074892cc24b0e2d933181a6bc6c2874c3
bernini <bernini@inventati.org>
2020-12-28 15:47:08 +0100

    improved intent_creation

--- lib/web/components/intent_create_activity/intent_create_activity_live.ex
+++ lib/web/components/intent_create_activity/intent_create_activity_live.ex
@@ -1,4 +1,16 @@
 defmodule Bonfire.UI.ValueFlows.IntentCreateActivityLive do
   use Bonfire.Web, :live_component
 
+  def mount(socket) do
+    {:ok, socket
+    |> assign(selected_tab: "")}
+  end
+
+
+  def handle_event("toggleProposal", %{"id" => id}, socket) do
+
+     socket = assign(socket, selected_tab: id)
+
+     {:noreply, socket}
+  end
 end
