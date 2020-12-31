a5b371e6c0d95f593158dacadf1926fc4b19daa8
Mayel <git@mayel.space>
2020-12-31 14:02:46 +0100

    refactor configs +

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.UI.ValueFlows.Integration do
 
-  def repo, do: Bonfire.Common.Config.get_ext!(:bonfire_ui_valueflows, :repo_module)
+  def repo, do: Bonfire.Common.Config.get!(:repo_module)
 
-  def mailer, do: Bonfire.Common.Config.get_ext!(:bonfire_ui_valueflows, :mailer_module)
+  def mailer, do: Bonfire.Common.Config.get!(:mailer_module)
 
 end
