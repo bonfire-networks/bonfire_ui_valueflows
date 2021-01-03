5f8b1c33d8a16bd3adf1b77a2ef18f324ef9523c
Mayel <git@mayel.space>
2021-01-03 13:37:49 +0100

    use our layout

--- lib/web/pages/breadpub_home_live.ex
+++ lib/web/pages/breadpub_home_live.ex
@@ -1,27 +1,29 @@
 defmodule Bonfire.UI.ValueFlows.BreadpubHomeLive do
-  use Bonfire.Web, :live_view
+  use Bonfire.Web, {:live_view, [layout: {Bonfire.UI.ValueFlows.LayoutView, "live.html"}]}
+
   alias Bonfire.UI.Social.HashtagsLive
   alias Bonfire.UI.ValueFlows.IntentCreateActivityLive
   alias Bonfire.UI.ValueFlows.ProposalFeedLive
+
   alias Bonfire.Common.Web.LivePlugs
-    alias Bonfire.Me.Users
-    alias Bonfire.Me.Web.{CreateUserLive, MeHomeLive}
+  alias Bonfire.Me.Users
+  alias Bonfire.Me.Web.{CreateUserLive, MeHomeLive}
 
-    def mount(params, session, socket) do
-      LivePlugs.live_plug params, session, socket, [
-        LivePlugs.LoadCurrentAccount,
-        LivePlugs.LoadCurrentUser,
-        LivePlugs.StaticChanged,
-        LivePlugs.Csrf,
-        &mounted/3,
-      ]
-    end
+  def mount(params, session, socket) do
+    LivePlugs.live_plug params, session, socket, [
+      LivePlugs.LoadCurrentAccount,
+      LivePlugs.LoadCurrentUser,
+      LivePlugs.StaticChanged,
+      LivePlugs.Csrf,
+      &mounted/3,
+    ]
+  end
 
-    defp mounted(params, session, socket) do
-      {:ok, socket
-      |> assign(page_title: "Home",
-      selected_tab: "about",
-      )}
-    end
+  defp mounted(params, session, socket) do
+    {:ok, socket
+    |> assign(page_title: "Home",
+    selected_tab: "about",
+    )}
+  end
 
 end
