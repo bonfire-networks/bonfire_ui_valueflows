085924bd7df7f35ce5416c9ff88a4312c64968d3
Mayel <git@mayel.space>
2021-01-03 19:04:13 +0100

    create intent

--- lib/web/components/intent_create_activity/intent_create_activity_live.ex
+++ lib/web/components/intent_create_activity/intent_create_activity_live.ex
@@ -1,5 +1,7 @@
 defmodule Bonfire.UI.ValueFlows.IntentCreateActivityLive do
   use Bonfire.Web, :live_component
+  alias ValueFlows.Planning.Intent.Intents
+  alias Bonfire.Geolocate.Geolocations
 
   def mount(socket) do
     {:ok, socket
@@ -12,5 +14,35 @@ defmodule Bonfire.UI.ValueFlows.IntentCreateActivityLive do
      socket = assign(socket, selected_tab: id)
 
      {:noreply, socket}
+  end
+
+  def handle_event("create", attrs, socket) do
+
+      user = Map.get(socket.assigns, :current_user)
+
+      need = input_to_atoms(Map.get(attrs, "need"))
+
+      if Map.get(need, :name) do
+
+        address = Map.get(need, :at_location)
+        location = if(address) do
+          with {:ok, geo} <- Geolocations.create(user, %{name: address, mappable_address: address}) do
+            geo
+          end
+        end
+
+        data = need |> Map.merge(%{
+          action: "work",
+          is_public: true,
+          at_location: location,
+          receiver: location
+        })
+
+        {:ok, intent} = Intents.create(user, data)
+        IO.inspect(intent)
+
+      end
+
+      {:noreply, socket}
   end
 end
