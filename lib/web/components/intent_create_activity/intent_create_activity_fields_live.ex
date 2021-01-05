14f6916d272da7d44b5541e7bad663f909609586
bernini <bernini@inventati.org>
2021-01-05 16:33:44 +0100

    intent form

---
+++ lib/web/components/intent_create_activity/intent_create_activity_fields_live.ex
@@ -0,0 +1,52 @@
+defmodule Bonfire.UI.ValueFlows.IntentCreateActivityFieldsLive do
+  use Bonfire.Web, :live_component
+  alias ValueFlows.Planning.Intent.Intents
+  alias Bonfire.Geolocate.Geolocations
+  alias Bonfire.UI.Social.SelectAutocompleteLive
+
+  def mount(socket) do
+    {:ok, socket
+    |> assign(is_public: false )}
+  end
+
+  def handle_event("toggleIsPublic", _ , socket) do
+
+    socket = assign(socket, is_public: !socket.assigns.is_public)
+
+    {:noreply, socket}
+ end
+
+
+
+  def handle_event("create", attrs, socket) do
+
+      user = Map.get(socket.assigns, :current_user)
+
+      need = input_to_atoms(Map.get(attrs, "need"))
+
+      if Map.get(need, :name) do
+
+        address = Map.get(need, :at_location)
+        location = if(address) do
+          with {:ok, geo} <- Geolocations.create(user, %{name: address, mappable_address: address}) do
+            geo
+          end
+        end
+
+        data = need |> Map.merge(%{
+          action: "work",
+          is_public: socket.assigns.is_public,
+          at_location: location,
+          receiver: location
+        })
+
+        {:ok, intent} = Intents.create(user, data)
+        IO.inspect(intent)
+
+      end
+
+      {:noreply, socket}
+  end
+
+
+end
--- lib/web/components/intent_create_activity/intent_create_activity_live.ex
+++ lib/web/components/intent_create_activity/intent_create_activity_live.ex
@@ -1,48 +1,6 @@
 defmodule Bonfire.UI.ValueFlows.IntentCreateActivityLive do
   use Bonfire.Web, :live_component
-  alias ValueFlows.Planning.Intent.Intents
-  alias Bonfire.Geolocate.Geolocations
 
-  def mount(socket) do
-    {:ok, socket
-    |> assign(selected_tab: "")}
-  end
-
-
-  def handle_event("toggleProposal", %{"id" => id}, socket) do
-
-     socket = assign(socket, selected_tab: id)
-
-     {:noreply, socket}
-  end
-
-  def handle_event("create", attrs, socket) do
-
-      user = Map.get(socket.assigns, :current_user)
-
-      need = input_to_atoms(Map.get(attrs, "need"))
-
-      if Map.get(need, :name) do
-
-        address = Map.get(need, :at_location)
-        location = if(address) do
-          with {:ok, geo} <- Geolocations.create(user, %{name: address, mappable_address: address}) do
-            geo
-          end
-        end
-
-        data = need |> Map.merge(%{
-          action: "work",
-          is_public: true,
-          at_location: location,
-          receiver: location
-        })
-
-        {:ok, intent} = Intents.create(user, data)
-        IO.inspect(intent)
-
-      end
-
-      {:noreply, socket}
-  end
+  alias Bonfire.UI.ValueFlows.IntentCreateActivityFieldsLive
+ 
 end
