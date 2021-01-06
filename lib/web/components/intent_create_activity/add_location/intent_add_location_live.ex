beca4d4ce4260dd49e50227464e6f02f89c263a9
bernini <bernini@inventati.org>
2021-01-06 11:19:34 +0100

    add_location

---
+++ lib/web/components/intent_create_activity/add_location/intent_add_location_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.UI.ValueFlows.IntentAddLocationLive do
+  use Bonfire.Web, :live_component
+
+end
--- lib/web/components/intent_create_activity/intent_create_activity_fields_live.ex
+++ lib/web/components/intent_create_activity/intent_create_activity_fields_live.ex
@@ -2,23 +2,95 @@ defmodule Bonfire.UI.ValueFlows.IntentCreateActivityFieldsLive do
   use Bonfire.Web, :live_component
   alias ValueFlows.Planning.Intent.Intents
   alias Bonfire.Geolocate.Geolocations
-  alias Bonfire.UI.Social.SelectAutocompleteLive
+  alias Bonfire.UI.ValueFlows.IntentAddLocationLive
 
   def mount(socket) do
-    {:ok, loc} = Geolocations.many()
-    locations = Enum.map(loc, fn (x) -> Map.take(x, [:name, :id]) end)
+   
     {:ok, socket
-    |> assign(is_public: false, locations: locations )}
+    |> assign(
+      toggled_location: false,
+      is_public: false, 
+      at_location: "",
+      search_locations_results: [],
+      search_location_phrase: "" )}
   end
 
   def handle_event("toggleIsPublic", _ , socket) do
-
     socket = assign(socket, is_public: !socket.assigns.is_public)
-
     {:noreply, socket}
  end
 
 
+ def handle_event("toggle_location", _ , socket) do
+  socket = assign(socket, toggled_location: !socket.assigns.toggled_location)
+  {:noreply, socket}
+  end
+
+
+
+
+  # START FUNCTIONS FOR INTENT ADD LOCATION FORM
+  # ive tried to move them all in the intent_add_location_live module, making it stateful, 
+  # but I need the at_location param in the parent compoent (this one), to be used when creating the new intent
+  # it seems send(self) only works with the parent view and not with the parent component
+  # maybe wrong? 
+
+  def handle_event("clear_location", _, socket) do
+    assigns = [
+      at_location: ""
+    ]
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("search_location", %{"at_location" => at_location}, socket) do
+    IO.inspect(at_location)
+    {:ok, loc} = Geolocations.many()
+    locations = Enum.map(loc, fn (x) -> Map.take(x, [:name, :id]) end)
+    assigns = [
+      search_locations_results: search(Enum.map(locations, fn x -> x.name end), at_location),
+      search_location_phrase: at_location
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def search(""), do: []
+
+  def search(list, prefix) do
+    Enum.filter(list, &has_prefix?(&1, prefix))
+  end
+
+  defp has_prefix?(item, prefix) do
+    String.starts_with?(String.downcase(item), String.downcase(prefix))
+  end
+
+  def handle_event("pick_location", %{"name" => picked}, socket) do
+    IO.inspect(picked)
+
+    assigns = [
+      search_locations_results: [],
+      search_location_phrase: "",
+      at_location: picked,
+      toggled_location: false
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("create_location", %{"at_location" => picked}, socket) do
+    IO.inspect(picked)
+
+    assigns = [
+      search_locations_results: [],
+      search_location_phrase: "",
+      at_location: picked,
+      toggled_location: false
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  # END FUNCTIONS FOR INTENT ADD LOCATIONS
 
   def handle_event("create", attrs, socket) do
 
