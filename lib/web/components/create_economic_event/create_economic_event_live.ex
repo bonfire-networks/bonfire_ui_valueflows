43f70f0d368e4c6bd3be4cd543d4fb9cd70cb477
ivan <bernini@inventati.org>
2021-04-29 13:24:13 +0200

    create economic event form

--- lib/web/components/create_economic_event/create_economic_event_live.ex
+++ lib/web/components/create_economic_event/create_economic_event_live.ex
@@ -1,4 +1,6 @@
 defmodule Bonfire.UI.ValueFlows.CreateEconomicEventLive do
   use Bonfire.Web, :stateless_component
 
+  prop action, :string, required: true
+  prop remove, :string, required: true
 end
---
+++ lib/web/components/select_economic_event/select_economic_event_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.UI.ValueFlows.SelectEconomicEventLive do
+  use Bonfire.Web, :stateless_component
+
+end
