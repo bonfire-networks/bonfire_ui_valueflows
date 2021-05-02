d301a9ec5c6cfec3ea0075d04d8440e6eeb72560
ivan <bernini@inventati.org>
2021-05-02 21:18:09 +0200

    improvement process page

--- lib/web/components/create_intent/create_intent_live.ex
+++ lib/web/components/create_intent/create_intent_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.UI.ValueFlows.CreateIntentLive do
   use Bonfire.Web, :stateless_component
 
-  prop intent_url, :string
+  prop intent_url, :string, required: false, default: ""
   prop action, :string, required: false
   prop output_of_id, :string, required: false
   prop provider_id, :string, required: false
