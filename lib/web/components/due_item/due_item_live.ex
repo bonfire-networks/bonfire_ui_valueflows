b46f51852734a971dd31d4b779aafcdcdf528513
ivan <bernini@inventati.org>
2021-05-03 16:31:10 +0200

    edit assigned_to and edit due_time component

--- lib/web/components/due_item/due_item_live.ex
+++ lib/web/components/due_item/due_item_live.ex
@@ -1,5 +1,6 @@
 defmodule Bonfire.UI.ValueFlows.DueItemLive do
   use Bonfire.Web, :stateless_component
+  alias Surface.Components.Form.DateTimeLocalInput
 
   prop date, :string, required: true
   prop is_editable, :boolean, default: false
