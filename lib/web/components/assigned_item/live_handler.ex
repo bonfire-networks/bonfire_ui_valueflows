b521a56bb63647e1dccbf8120aa7f905236a9015
Mayel <git@mayel.space>
2021-05-06 18:39:28 +0200

    assign intents

---
+++ lib/web/components/assigned_item/live_handler.ex
@@ -0,0 +1,24 @@
+defmodule Bonfire.UI.ValueFlows.AssignedItemLive.LiveHandler do
+  import Bonfire.Common.Utils
+
+  def handle_event("select", %{"id" => id, "name"=>name} = _attrs, socket) when is_binary(id) do
+
+    {:noreply,
+        socket
+        |> cast_self(
+          assign_to: [{name, id}] |> IO.inspect
+        )
+    }
+  end
+
+  def handle_event("deselect", %{"id" => _deselected} = _attrs, socket) do
+
+    {:noreply,
+        socket
+        |> cast_self(
+          assign_to: []
+        )
+    }
+  end
+
+end
