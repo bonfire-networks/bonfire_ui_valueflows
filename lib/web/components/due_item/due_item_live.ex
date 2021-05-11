976531895ea4bc713b34caef6f8732caee3bedba
ivan <bernini@inventati.org>
2021-05-11 10:45:28 +0200

    minor ui and generalized components

--- lib/web/components/due_item/due_item_live.ex
+++ lib/web/components/due_item/due_item_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.ValueFlows.DueItemLive do
   use Bonfire.Web, :stateless_component
-  alias Surface.Components.Form.DateTimeLocalInput
+  alias Surface.Components.Form.DateInput
 
   prop date, :string, required: true
   prop is_editable, :boolean, default: false
---
+++ lib/web/components/edit_description/edit_description_live.ex
@@ -0,0 +1,5 @@
+defmodule Bonfire.UI.ValueFlows.EditDescriptionLive do
+  use Bonfire.Web, :stateless_component
+
+  prop item, :map
+end
