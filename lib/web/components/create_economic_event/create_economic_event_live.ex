0ada3a8096a5a2ac53c5891b40ad5425889a5647
bernini <bernini@inventati.org>
2021-07-06 18:32:14 +0200

    units

--- lib/web/components/create_economic_event/create_economic_event_live.ex
+++ lib/web/components/create_economic_event/create_economic_event_live.ex
@@ -4,4 +4,5 @@ defmodule Bonfire.UI.ValueFlows.CreateEconomicEventLive do
   prop action, :string, required: true
   prop remove, :string, required: true
   prop output_of_id, :string
+  prop units, :list
 end
--- lib/web/components/select_economic_event/select_economic_event_live.ex
+++ lib/web/components/select_economic_event/select_economic_event_live.ex
@@ -2,4 +2,5 @@ defmodule Bonfire.UI.ValueFlows.SelectEconomicEventLive do
   use Bonfire.Web, :stateless_component
 
   prop output_of_id, :string
+  prop units, :list
 end
