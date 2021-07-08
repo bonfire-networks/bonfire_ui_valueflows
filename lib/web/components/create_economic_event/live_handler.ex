29310b036ec4b726bee7864a0870fbb3ccd1505a
Mayel <git@mayel.space>
2021-07-08 20:38:24 +0200

    autocomplete/search

--- lib/web/components/create_economic_event/live_handler.ex
+++ lib/web/components/create_economic_event/live_handler.ex
@@ -1,6 +1,7 @@
 defmodule Bonfire.UI.ValueFlows.CreateEconomicEventLive.LiveHandler do
   use Bonfire.Web, :live_handler
 
+  #FIXME: refactor if/when LiveView supports even on field rather than on form only
 
   def handle_event("autocomplete"=action, %{"autocomplete_resource_conforms_to" => search} = attrs, socket) when is_binary(search) and byte_size(search)>0 do
     ValueFlows.Knowledge.ResourceSpecification.LiveHandler.handle_event(action, search, socket)
@@ -12,6 +13,10 @@ defmodule Bonfire.UI.ValueFlows.CreateEconomicEventLive.LiveHandler do
 
   def handle_event("autocomplete"=action, %{"autocomplete_at_location" => search} = attrs, socket) when is_binary(search) and byte_size(search)>0 do
     Bonfire.Geolocate.LiveHandler.handle_event(action, search, socket)
+  end
+
+  def handle_event("autocomplete"=action, %{"autocomplete_receiver" => search} = attrs, socket) when is_binary(search) and byte_size(search)>0 do
+    Bonfire.Me.Users.LiveHandler.handle_event(action, search, socket)
   end
 
   def handle_event(_, _, socket) do
