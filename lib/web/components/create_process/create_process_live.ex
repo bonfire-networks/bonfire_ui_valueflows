7d5ed047d771e43b662c8787fd25d78298a05e23
bernini <bernini@inventati.org>
2021-07-08 15:47:00 +0200

    smart input shows default activity

--- lib/web/components/create_process/create_process_live.ex
+++ lib/web/components/create_process/create_process_live.ex
@@ -1,6 +1,7 @@
 defmodule Bonfire.UI.ValueFlows.CreateProcessLive do
   use Bonfire.Web, :stateless_component
 
+  prop close, :string
   prop process_url, :string
 
 end
---
+++ lib/web/components/create_process_smart_input/create_process_smart_input_live.ex
@@ -0,0 +1,6 @@
+defmodule Bonfire.UI.ValueFlows.CreateProcessSmartInputLive do
+  use Bonfire.Web, :stateless_component
+
+  prop process_url, :string
+
+end
