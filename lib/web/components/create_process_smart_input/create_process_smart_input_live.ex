193b16aa0549947c838ada0c85213c19b66b9931
Mayel de Borniol <git@mayel.space>
2021-12-12 19:54:43 +1300

    process_url

--- lib/web/components/create_process_smart_input/create_process_smart_input_live.ex
+++ lib/web/components/create_process_smart_input/create_process_smart_input_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.ValueFlows.CreateProcessSmartInputLive do
   use Bonfire.Web, :stateless_component
 
-  prop process_url, :string
+  prop process_url, :string, default: "/process/"
 
 end
