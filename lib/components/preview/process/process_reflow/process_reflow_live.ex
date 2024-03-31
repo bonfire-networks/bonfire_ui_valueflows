8452eee928f9d8050d3d6503a6e3ffc0b440d580
Mayel de Borniol <git@mayel.space>
2024-03-31 19:26:28 +0100

    fix

--- lib/components/preview/process/process_reflow/process_reflow_live.ex
+++
@@ -1,9 +0,0 @@
-defmodule Bonfire.UI.Reflow.Preview.ProcessReflowLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  # alias Bonfire.Social.Likes
-
-  prop object, :any, required: true
-  prop activity, :any, default: nil
-  prop showing_within, :atom, default: nil
-end
