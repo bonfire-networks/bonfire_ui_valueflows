f4f6868e6a177e83b0ac24273b3477ede3d705c0
Mayel de Borniol <git@mayel.space>
2024-04-03 14:08:23 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,4 +1,12 @@
 defmodule Bonfire.UI.ValueFlows.Integration do
+  use Bonfire.Common.Utils
+
+  declare_extension("Common UI components for ValueFlows",
+    icon: "fluent-mdl2:web-components",
+    emoji: "🧩",
+    description: l("Reusable user interface components for other ValueFlows extensions.")
+  )
+
   def repo, do: Bonfire.Common.Config.repo()
 
   def mailer, do: Bonfire.Common.Config.get!(:mailer_module)
